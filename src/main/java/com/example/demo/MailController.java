f260681576e2ad56c529b6f7cef7cec3c91781be
MikhailVilchyk <kate.classical@gmail.com>
2020-04-16 14:57:57 +0300

    added path for image.jpg

--- src/main/java/com/example/demo/MailController.java
+++ src/main/java/com/example/demo/MailController.java
@@ -33,9 +33,10 @@ public class MailController {
 
         URL imageUrl;
         try {
-            imageUrl = new URL("http://www.401khelpcenter.com/syndicate/img/AF-Logo_Brown_Small.gif");
+            //imageUrl = new URL("http://www.401khelpcenter.com/syndicate/img/AF-Logo_Brown_Small.gif");
             //imageUrl = new URL("https://www.w3schools.com/images/picture.jpg");
             //imageUrl = new URL("https://www.google.com/images/srpr/logo11w.png");
+            imageUrl = new URL("https://ktonanovenkogo.ru/image/finik.jpg");
         } catch (MalformedURLException ex) {
             throw new RuntimeException(ex);
         }
@@ -60,6 +61,18 @@ public class MailController {
         log.info("!!! GET test template={}, phone={}, inn={}", template, phone, inn);
         System.out.println("!!! GET test");
 
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping(value = "/pixel")
+    public ResponseEntity<?> getPixel(){
+        log.info("!!! GET test pixel");
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping(value = "/pixel.jpg")
+    public ResponseEntity<?> getPixelJPG(){
+        log.info("!!! GET test pixel.jpg");
         return ResponseEntity.ok().build();
     }
 }
